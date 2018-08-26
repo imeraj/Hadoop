4ce3d4e64ce6f443f7e93872234567bf1781cdac
Meraj <meraj.enigma@gmail.com>
2018-08-26 19:59:19 +0900

    All sqoop jobs defined
    Signed-off-by: Meraj <meraj.enigma@gmail.com>

--- projects/bank/sqoop_jobs.sh
+++ projects/bank/sqoop_jobs.sh
@@ -1,10 +1,12 @@
-hadoop fs -mkdir /bank
 hadoop fs -rm -r /bank
+hadoop fs -mkdir /bank
 
 sqoop job --delete loan_info_job
-hadoop fs -mkdir /bank/loan_info_stg
+sqoop job --delete credit_card_info_job 
+sqoop job --delete shares_info_job
 
 export HADOOP_CLASSPATH=$HADOOP_CLASSPATH:libjars/
+
 sqoop job --create loan_info_job \
 -- \
 import --connect 'jdbc:mysql://localhost/bank?autoReconnect=true&useSSL=false' \
@@ -17,8 +19,35 @@ import --connect 'jdbc:mysql://localhost/bank?autoReconnect=true&useSSL=false' \
 --check-column loan_id \
 --m 1;
 
-sqoop job --exec loan_info_job
+sqoop job --create credit_card_info_job \
+-- \
+import --connect 'jdbc:mysql://localhost/bank?autoReconnect=true&useSSL=false' \
+--username 'myuser' \
+--password-file 'file:///Users/meraj/Development/Hadoop/projects/bank/sqoop_password' \
+--bindir ./libjars \
+--table 'credit_card_info' \
+--target-dir '/bank/credit_card_info_stg' \
+--incremental append \
+--check-column cc_number \
+-m 1;
+
+sqoop job --create shares_info_job \
+-- \
+import --connect 'jdbc:mysql://localhost/bank?autoReconnect=true&useSSL=false' \
+--username 'myuser' \
+--password-file 'file:///Users/meraj/Development/Hadoop/projects/bank/sqoop_password' \
+--bindir ./libjars \
+--table 'shares_info' \
+--target-dir '/bank/shares_info_stg' \
+-m 1;
+
 sqoop job --list
 
+sqoop job --exec loan_info_job
+sqoop job --exec credit_card_info_job
+sqoop job --exec shares_info_job 
+
 hadoop fs -cat "/bank/loan_info_stg/*"
+hadoop fs -cat "/bank/credit_card_info_stg/*"
+hadoop fs -cat "/bank/shares_info_stg/*"
 
